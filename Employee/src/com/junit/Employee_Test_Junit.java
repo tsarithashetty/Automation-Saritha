2a832d0efe72c966f82c5442cd2b882cfab4f26a
tsarithashetty <45058146+tsarithashetty@users.noreply.github.com>
2018-11-27 12:10:24 +0530

    Add files via upload

---
+++ Employee/src/com/junit/Employee_Test_Junit.java
@@ -0,0 +1,39 @@
+package com.junit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class Employee_Test_Junit {
+   EmployeeDetails emp;
+    EmployeeDetails_Test emtest;
+
+    public Employee_Test_Junit() {
+        emp = new EmployeeDetails();
+        emtest = new EmployeeDetails_Test();
+    }
+
+    //test to check appraisal
+    @Test
+    public void testCalculateAppraisal() {
+        emp.setName("Tom");
+        emp.setAge(25);
+        emp.setMonthlySalary(2000);
+
+        double ann_sal = emtest.CalculateYearlySalary(emp);
+        assertEquals(24000, ann_sal, 0.0);
+    }
+
+    // test to check yearly salary
+    @Test
+    public void testCalculateYearlySalary() {
+        emp.setName("Tom");
+        emp.setAge(25);
+        emp.setMonthlySalary(3000);
+
+        double salary = emtest.CalculateAppraisal(emp);
+        assertEquals(2000, salary, 0.0);
+    }
+}
